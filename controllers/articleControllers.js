bf9fa651da179ad7d24532dd7bb3efccd6cf9b49
Fanny <fannyy.paris@gmail.com>
2022-08-20 15:22:52 +0200

    feat(article)CRUD

--- controllers/articleControllers.js
+++ controllers/articleControllers.js
@@ -4,8 +4,7 @@ const fs = require('fs');
 exports.createArticle = async (req, res, next) =>{
     try{
         const articleObject = req.body;
-        console.log(articleObject);
-
+      
         const article = await Article.create({
             ...articleObject,
             picture: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
@@ -18,5 +17,76 @@ exports.createArticle = async (req, res, next) =>{
     catch (error){
         console.log(error);
         res.status(400).json({error})
+    }
+}
+
+exports.modifyArticle = async (req, res, next) => {
+    try{
+        const article = await Article.findOne({_id: req.params.id});
+
+        const articleObject = req.body;
+
+        let filename = null;
+        if(req.file){
+            filename = article.picture.split('/images/')[1];
+        }
+       
+        fs.unlink(`images/${filename}`, async () =>{
+            await Article.updateOne({_id: req.params.id}, {
+                picture: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+                ...articleObject
+            })
+
+            res.status(201).json({message: "The article has been updated !"})
+        })
+
+    }
+
+    catch(error){
+        console.log(error);
+        res.status(400).json({error});
+    }
+}
+
+exports.deleteArticle = async (req, res, next) =>{
+    try{
+
+        const article = await Article.findOne({_id: req.params.id});
+
+        const filename = article.picture.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+            Article.deleteOne({_id: req.params.id})
+            .then(() => {
+                res.status(200).json({message: "The article has been deleted !"});
+            })
+            .catch((error)=>{
+                res.status(400).json({error});
+            })
+        })
+
+    }
+
+    catch(error){
+        console.log(error);
+        res.status(400).json({error});
+    }
+}
+
+exports.getAllArticles = async (req, res, next) =>{
+    Article.find()
+    .then((article => res.status(200).json(article)))
+    .catch((error => res.status(400).json({error})))
+
+}
+
+exports.getOneArticle = async (req, res, next) => {
+    try{
+        const article = await Article.findOne({_id: req.params.id})
+        res.status(200).json(article);
+    }
+
+    catch(error){
+        console.log(error);
+        res.status(400).json({error});
     }
 }
--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -4,5 +4,10 @@ const articleCtrl = require('../controllers/articleControllers');
 const multer = require('../middlewares/multer');
 
 router.post('/create', multer, articleCtrl.createArticle);
+router.get('/getAllArticles', articleCtrl.getAllArticles);
+router.get('/getOneArticle/:id', articleCtrl.getOneArticle);
+router.put('/getOneArticle/:id', multer, articleCtrl.modifyArticle);
+router.delete('/getOneArticle/:id', multer, articleCtrl.deleteArticle);
+
 
 module.exports = router;
